1c78fa35ef179dda1c6ec7afc37bf57ef338de74
Boeing737ng <ghlove0317@naver.com>
2020-06-25 15:46:46 +0900

    Initial Commit

---
+++ photoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoApp
+//
+//  Created by Kihyun Choi on 2020/06/25.
+//  Copyright © 2020 kakaopay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

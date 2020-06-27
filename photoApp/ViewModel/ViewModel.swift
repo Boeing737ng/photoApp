581dfdec8813e996ba2bdba0ed3510c9dd0d3f07
Boeing737ng <ghlove0317@naver.com>
2020-06-27 12:19:05 +0900

    Viewmodel pattern? applied

---
+++ photoApp/ViewModel/ViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  ViewModel.swift
+//  photoApp
+//
+//  Created by Kihyun Choi on 2020/06/27.
+//  Copyright © 2020 kakaopay. All rights reserved.
+//
+
+import UIKit
+
+struct ImageForCell {
+    let image: UIImage
+}
+
+class ViewModel {
+    // MARK: Properties
+    var mode:String?
+    var photos: [ImageDetail]? {
+        didSet {
+            self.fetchPhoto()
+        }
+    }
+    var imageForCell: [ImageForCell] = []
+    
+    var isLodingData:Bool = false {
+        didSet {
+            showLoading?()
+        }
+    }
+
+    var showLoading: (() -> Void)?
+    var reloadData: (() -> Void)?
+    var showError: ((Error) -> Void)?
+
+    init() {
+        
+    }
+
+    func fetchAllPhotos(page:Int, per_page:Int) {
+        self.isLodingData = true
+        let imageRequest = DataRequest(page: page, per_page: per_page)
+        imageRequest.getImageData { [weak self] result in
+            switch result {
+            case .failure(let error):
+                self?.showError?(error)
+            case .success(let images):
+                self?.photos = images
+            }
+        }
+    }
+    
+    func fetchQueryPhotos(query:String, page:Int) {
+        self.isLodingData = true
+        let imageRequest = SearchRequest(searchStr:query, page: page)
+        imageRequest.getImageData { [weak self] result in
+            switch result {
+            case .failure(let error):
+                self?.showError?(error)
+            case .success(let images):
+                self?.photos = images
+            }
+        }
+    }
+
+    private func fetchPhoto() {
+        let group = DispatchGroup()
+        DispatchQueue.global(qos: .background).async(group: group) {
+            group.enter()
+            self.photos?.forEach { (photo) in
+                guard let imageData = try? Data(contentsOf: photo.urls.thumb) else {
+                    self.showError?(UnsplashError.noDataAvailable)
+                    return
+                }
+
+                guard let image = UIImage(data: imageData) else {
+                    self.showError?(UnsplashError.canNotProcessData)
+                    return
+                }
+
+                self.imageForCell.append(ImageForCell(image: image))
+            }
+            group.leave()
+        }
+        group.notify(queue: .main) {
+            self.isLodingData = false
+            self.reloadData?()
+        }
+    }
+}
